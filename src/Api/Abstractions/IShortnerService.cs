f8fcd0125bdbc91ee29c66edc1e47d8da4c4076e
Alexandr Pilipenko <altafard@gmail.com>
2019-01-24 12:40:26 +0300

    Create an abstraction for working with URLs

---
+++ src/Api/Abstractions/IShortnerService.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Abstractions
+{
+    public interface IShortenerService
+    {
+        string Create(string origin);
+
+        string Obtain(string value);
+
+        int GetClicks(string value);
+    }
+}
