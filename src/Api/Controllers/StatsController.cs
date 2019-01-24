a313a0ed75d103759a08205922e598f60697f166
Alexandr Pilipenko <altafard@gmail.com>
2019-01-24 12:30:39 +0300

    Add endpoints

---
+++ src/Api/Controllers/StatsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class StatsController : ControllerBase
+    {
+        [HttpGet]
+        public int Get(string shortUrl)
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/Api/Controllers/UrlController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController, Route("api/[controller]")]
+    public class UrlController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string shortUrl)
+        {
+            return "long";
+        }
+
+        [HttpPost]
+        public string Post([FromBody] string longUrl)
+        {
+            return "short";
+        }
+    }
+}
